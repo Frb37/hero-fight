2b960ba7a24d0a6556221d2ff5d2e1f76093360b
Frb37 <43365836+Frb37@users.noreply.github.com>
2024-10-28 14:20:09 +0100

    Seeder (#2)
    * feat: created seeder files to populate DB
    
    * feat: working on CRUD for users and characters but I need to hydrate the DB first

--- app/Views/admin/dashboard/index.php
+++ app/Views/admin/dashboard/index.php
@@ -1,2 +1,9 @@
 <?php
-echo "coucou";
+/*var_dump($user->id, $user->username, $user->email);
+*/?>
+
+<div class="card">
+    <div class="card-header">
+      <h1> Bienvenue, <?= isset($user)? $user->username: 'invité'; ?> ! </h1>
+    </div>
+</div>
